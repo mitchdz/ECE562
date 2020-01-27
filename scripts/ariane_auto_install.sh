934192ba61818dc2a83c0c3066c413d90b9a650c
Lena Voytek <dvoytek@email.arizona.edu>
2020-01-27 13:40:29 -0700

    Cleans directory on reinstall

--- scripts/ariane_auto_install.sh
+++ scripts/ariane_auto_install.sh
@@ -6,6 +6,8 @@ if [[ "$*" == *--sudo* ]]; then
 	sudo apt install -y autoconf automake autotools-dev curl libmpc-dev libmpfr-dev libgmp-dev gawk git build-essential bison flex texinfo gperf libtool patchutils bc zlib1g-dev libexpat-dev libusb-1.0-0-dev device-tree-compiler pkg-config libexpat-dev verilator
 fi
 
+rm -rf ~/riscv
+
 mkdir ~/riscv
 
 pushd ~/riscv
