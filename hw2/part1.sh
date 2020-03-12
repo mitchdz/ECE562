d318436e83c305af21296c2341b4619fc6511ae0
mitchell dzurick <mitch_dz@hotmail.com>
2020-03-12 09:10:56 -0700

    hw2/m5out: init commit
    Signed-off-by: mitchell dzurick <mitch_dz@hotmail.com>

--- hw2/part1.sh
+++ hw2/part1.sh
@@ -1,28 +1,30 @@
 
-
+PROG="build/ARM/gem5.opt"
 
 declare -a arr=("a2time01" "cacheb01" "bitmnp01" "mcf" "libquantum")
 
 for i in "${arr[@]}"
 do
-	build/ARM/gem5.fast \
-		--stats-file=OoO_${i}stats.txt \
-		--dump-config=OoO${i}_config.ini \
+	${PROG} \
+		--stats-file=OoO_${i}_stats.txt \
+		--dump-config=OoO_${i}_config.ini \
 		configs/example/se.py \
 		--caches \
 		--l1i_size=32kB \
 		--l1i_assoc=4 \
-		--l1d_size=32kB --l1d_assoc=4 --cacheline_size=64 --l2cache --l2_size=1MB --l2_assoc=8 --cpu- \
-		clock=1.6GHz --cpu-type=ex5_big -n 1 --maxinsts=100000000 --bench ${i}
+		--l1d_size=32kB --l1d_assoc=4 --cacheline_size=64 --l2cache \
+		--l2_size=1MB --l2_assoc=8 --cpu-clock=1.6GHz \
+		--cpu-type=ex5_big -n 1 --maxinsts=100000000 --bench ${i}
 
-	build/ARM/gem5.fast \
-		--stats-file=IoO_${i}stats.txt \
-		--dump-config=IoO${i}_config.ini \
+	${PROG} \
+		--stats-file=IoO_${i}_stats.txt \
+		--dump-config=IoO_${i}_config.ini \
 		configs/example/se.py \
 		--caches \
 		--l1i_size=32kB \
 		--l1i_assoc=4 \
-		--l1d_size=32kB --l1d_assoc=4 --cacheline_size=64 --l2cache --l2_size=1MB --l2_assoc=8 --cpu- \
-		clock=1.6GHz --cpu-type=ex5_LITTLE -n 1 --maxinsts=100000000 --bench ${i}
+		--l1d_size=32kB --l1d_assoc=4 --cacheline_size=64 --l2cache \
+		--l2_size=1MB --l2_assoc=8 --cpu-clock=1.6GHz \
+		--cpu-type=ex5_LITTLE -n 1 --maxinsts=100000000 --bench ${i}
 
 done
